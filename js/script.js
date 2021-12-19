2d2c7aded8634333475e043fd332ed9654887a35
Avi Dalal <avi413@gmail.com>
2021-12-19 20:26:48 +0200

    butify js

--- js/script.js
+++ js/script.js
@@ -5,27 +5,27 @@ let profileName = document.querySelector(".profile__name");
 let profileAboutMe = document.querySelector(".profile__about-me");
 let inputName = document.querySelector(".popup__input-text_type_name");
 let inputAboutMe = document.querySelector(".popup__input-text_type_about-me");
-
-
-editBtn.addEventListener("click", popupCloseBtn );
-
-popupClose.addEventListener("click", function (evt) {
-    popup.classList.add("popup_state_closed");
-})
-
 let formElement = document.querySelector(".popup__form");
 
+editBtn.addEventListener("click", handleOpenEditProfile );
+popupClose.addEventListener("click", handleClodeEditProfile);
+formElement.addEventListener('submit', handleProfileFormSubmit); 
+
+
 function handleProfileFormSubmit(evt) {
-    evt.preventDefault();
-    
+    evt.preventDefault(); 
     profileName.textContent  = inputName.value;
     profileAboutMe.textContent  =  inputAboutMe.value;
     popup.classList.add("popup_state_closed");
 }
 
-function popupCloseBtn() {
+function handleClodeEditProfile(evt) {
+    popup.classList.add("popup_state_closed");
+}
+
+function handleOpenEditProfile() {
     inputName.value  = profileName.textContent;
     inputAboutMe.value  =  profileAboutMe.textContent;
     popup.classList.remove("popup_state_closed");
 }
-formElement.addEventListener('submit', handleProfileFormSubmit); 
+
