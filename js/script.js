4b664bec470b08d95fa54afba5f80f3bf581712d
Avi Dalal <avi413@gmail.com>
2021-12-31 01:34:12 +0200

    feat(popup) add new card popup element

--- js/script.js
+++ js/script.js
@@ -1,58 +1,23 @@
-let editBtn = document.querySelector("button.profile__edit-btn");
-let popupClose = document.querySelector("button.popup__close");
-let popup = document.querySelector(".popup");
-let profileName = document.querySelector(".profile__name");
-let profileAboutMe = document.querySelector(".profile__about-me");
-let inputName = document.querySelector(".popup__input-text_type_name");
-let inputAboutMe = document.querySelector(".popup__input-text_type_about-me");
-let formElement = document.querySelector(".popup__form");
+const editBtn = document.querySelector("button.profile__edit-btn");
+const opeNewCardPopupBtn = document.querySelector("button.profile__add-btn");
+
+const popupProfile = document.querySelector(".popup_type_profile");
+const popupNewCard = document.querySelector(".popup_type_new-card");
+const popupCloseProfile = popupProfile.querySelector("button.popup__close_place_profile");
+const popupCloseNewCard = popupNewCard.querySelector("button.popup__close_place_new-card");
+const profileName = document.querySelector(".profile__name");
+const profileAboutMe = document.querySelector(".profile__about-me");
+const inputName = document.querySelector(".popup__input-text_type_name");
+const inputAboutMe = document.querySelector(".popup__input-text_type_about-me");
+const formElement = document.querySelector(".popup__form");
+
+
+
 
 const container = document.querySelector(".galery");
 const galeryListContainer = container.querySelector(".galery__list");
-const addButton = document.querySelector(".profile__add-btn");
+//const addButton = document.querySelector(".profile__add-btn");
 
-editBtn.addEventListener("click", handleOpenEditProfile );
-popupClose.addEventListener("click", handleClodeEditProfile);
-formElement.addEventListener('submit', handleProfileFormSubmit); 
-
-
-function handleProfileFormSubmit(evt) {
-    evt.preventDefault(); 
-    profileName.textContent = inputName.value;
-    profileAboutMe.textContent = inputAboutMe.value;
-    popup.classList.add("popup_closed");
-}
-
-function handleClodeEditProfile(evt) {
-    popup.classList.add("popup_closed");
-}
-
-function handleOpenEditProfile() {
-    inputName.value  = profileName.textContent;
-    inputAboutMe.value  =  profileAboutMe.textContent;
-    popup.classList.remove("popup_closed");
-}
-
-
-function addGaleryCardItem(imgSrctValue, titleValue) {
-    const itemTemplate = document.querySelector("#galery-item-template").content;
-    const galeryitemEl = itemTemplate.querySelector('.galery__item').cloneNode(true);
-  
-    galeryitemEl.querySelector(".galery__item-name").textContent = titleValue;
-    galeryitemEl.querySelector(".galery__item-img").src = imgSrctValue;
-    
-    galeryListContainer.append(galeryitemEl); 
-  }
-  
-  addButton.addEventListener("click", function () {
-    const title = "title";
-    const imgSrctValue = "./images/latemar.png";
-  
-    addGaleryCardItem(imgSrctValue.value, title.value);
-  
-    artist.value = "";
-    title.value = "";
-  });
 
 const initialCards = [
     {
@@ -79,8 +44,60 @@ const initialCards = [
       name: "Lago di Braies",
       link: "https://code.s3.yandex.net/web-code/lago.jpg"
     }
-  ]; 
+]; 
 
-  initialCards.forEach(element => {
+initialCards.forEach(element => {
     addGaleryCardItem(element.link,element.name);
 });
+
+
+
+editBtn.addEventListener("click", handleOpenEditProfile );
+opeNewCardPopupBtn.addEventListener("click", handleOpenAddCard );
+popupCloseProfile.addEventListener("click", handleClodeEditProfile);
+popupCloseNewCard.addEventListener("click", handleClodeEditProfile);
+formElement.addEventListener('submit', handleProfileFormSubmit); 
+
+function handleProfileFormSubmit(evt) {
+    evt.preventDefault(); 
+    profileName.textContent = inputName.value;
+    profileAboutMe.textContent = inputAboutMe.value;
+    popupProfile.classList.add("popup_closed");
+}
+
+function handleClodeEditProfile(evt) {
+  const parent = evt.target.parentNode;
+  parent.classList.add("popup_closed");
+}
+
+function handleOpenEditProfile() {
+    inputName.value  = profileName.textContent;
+    inputAboutMe.value  =  profileAboutMe.textContent;
+    popupProfile.classList.remove("popup_closed");
+}
+
+function handleOpenAddCard() {
+  popupNewCard.classList.remove("popup_closed");
+}
+
+function addGaleryCardItem(imgSrctValue, titleValue) {
+    const itemTemplate = document.querySelector("#galery-item-template").content;
+    const galeryitemEl = itemTemplate.querySelector('.galery__item').cloneNode(true);
+  
+    galeryitemEl.querySelector(".galery__item-name").textContent = titleValue;
+    galeryitemEl.querySelector(".galery__item-img").src = imgSrctValue;
+    
+    galeryListContainer.prepend(galeryitemEl); 
+  }
+
+/*
+addButton.addEventListener("click", function () {
+    const title = "title";
+    const imgSrctValue = "./images/latemar.png";
+
+    addGaleryCardItem(imgSrctValue.value, title.value);
+
+    imgSrctValue.value = "";
+    title.value = "";
+});*/
+
