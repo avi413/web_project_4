49718f48813e5a7fce307575eb7857feb9c807f1
Avi Dalal <avi413@gmail.com>
2021-12-31 20:13:56 +0200

    feat(galery like button) add js button click listener

--- js/script.js
+++ js/script.js
@@ -1,65 +1,82 @@
-const initialCards = [
-  {
-    name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
-  },
-  {
-    name: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
-  },
-  {
-    name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
-  },
-  {
-    name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
-  },
-  {
-    name: "Vanoise National Park",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg"
-  }
-]; 
+const rebderCards = () => {
+  const initialCards = [
+    {
+      name: "Yosemite Valley",
+      link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    },
+    {
+      name: "Lake Louise",
+      link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    },
+    {
+      name: "Bald Mountains",
+      link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    },
+    {
+      name: "Latemar",
+      link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    },
+    {
+      name: "Vanoise National Park",
+      link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+    },
+    {
+      name: "Lago di Braies",
+      link: "https://code.s3.yandex.net/web-code/lago.jpg"
+    }
+  ]; 
+  initialCards.forEach(element => {
+      addGaleryCardItem(element.link,element.name);
+  });
+};
 
+const closePopup = (popuop) => {
+  popuop.classList.add("popup_closed");
+}
+
+const openPopup = (popuop) => {
+  popuop.classList.remove("popup_closed");
+}
+
+//main buttons
 const profileEditBtn      = document.querySelector("button.profile__edit-btn");
 const opeNewCardPopupBtn  = document.querySelector("button.profile__add-btn");
-const popupProfile        = document.querySelector(".popup_type_profile");
-const popupNewCard        = document.querySelector(".popup_type_new-card");
+
+//profile data elements
 const profileName         = document.querySelector(".profile__name");
 const profileAboutMe      = document.querySelector(".profile__about-me");
-const profileFormElement  = document.querySelector(".popup__form_type_profile");
-const newCardFormElement  = document.querySelector(".popup__form_type_new-card");
-const container           = document.querySelector(".galery");
 
+//galery container
+const container           = document.querySelector(".galery");
 const galeryListContainer = container.querySelector(".galery__list");
 
+//profile popup elements
+const profileFormElement  = document.querySelector(".popup__form_type_profile");
+const popupProfile        = document.querySelector(".popup_type_profile");
 const popupCloseProfile   = popupProfile.querySelector("button.popup__close_place_profile");
 const inputName           = popupProfile.querySelector(".popup__input-text_type_name");
 const inputAboutMe        = popupProfile.querySelector(".popup__input-text_type_about-me");
 
+//new galery cars popup elements
+const newCardFormElement  = document.querySelector(".popup__form_type_new-card");
+const popupNewCard        = document.querySelector(".popup_type_new-card");
 const popupCloseNewCard   = popupNewCard.querySelector("button.popup__close_place_new-card");
 const newCardBtn          = popupNewCard.querySelector(".popup__input-btn_place_new-card");
 const inputTitle          = popupNewCard.querySelector(".popup__input-text_type_title");
 const inputImageLink      = popupNewCard.querySelector(".popup__input-text_type_Image-link");
 
-
-//const addButton = document.querySelector(".profile__add-btn");
-
-
-initialCards.forEach(element => {
-    addGaleryCardItem(element.link,element.name);
+//popups open events
+profileEditBtn.addEventListener("click", () => {
+  inputName.value  = profileName.textContent;
+  inputAboutMe.value  =  profileAboutMe.textContent;
+  openPopup(popupProfile);
 });
 
+opeNewCardPopupBtn.addEventListener("click", () => openPopup(popupNewCard) );
 
-
-profileEditBtn.addEventListener("click", handleOpenEditProfile);
-opeNewCardPopupBtn.addEventListener("click", handleOpenAddCard );
-popupCloseProfile.addEventListener("click", handleClodeEditProfile);
-popupCloseNewCard.addEventListener("click", handleClodeEditProfile);
+//popups close events
+popupCloseProfile.addEventListener("click", () => closePopup(popupProfile));
+popupCloseNewCard.addEventListener("click", () => closePopup(popupNewCard));
 
 profileFormElement.addEventListener('submit', handleProfileFormSubmit); 
 newCardFormElement.addEventListener("submit", handleCreateNewCard);
@@ -69,41 +86,28 @@ function handleCreateNewCard(evt) {
     addGaleryCardItem(inputImageLink.value, inputTitle.value);
     inputImageLink.value = "";
     inputTitle.value = "";
-    popupNewCard.classList.add("popup_closed");
+    closePopup(popupNewCard);
 }
 
 function handleProfileFormSubmit(evt) {
     evt.preventDefault(); 
     profileName.textContent = inputName.value;
     profileAboutMe.textContent = inputAboutMe.value;
-    popupProfile.classList.add("popup_closed");
-}
-
-function handleClodeEditProfile(evt) {
-  const parent = evt.target.parentNode;
-  parent.classList.add("popup_closed");
-}
-
-function handleOpenEditProfile() {
-    inputName.value  = profileName.textContent;
-    inputAboutMe.value  =  profileAboutMe.textContent;
-    popupProfile.classList.remove("popup_closed");
-}
-
-function handleOpenAddCard() {
-  popupNewCard.classList.remove("popup_closed");
+    closePopup(popupProfile);
 }
 
 function addGaleryCardItem(imgSrctValue, titleValue) {
-    const itemTemplate = document.querySelector("#galery-item-template").content;
-    const galeryitemEl = itemTemplate.querySelector('.galery__item').cloneNode(true);
-  
-    galeryitemEl.querySelector(".galery__item-name").textContent = titleValue;
-    galeryitemEl.querySelector(".galery__item-img").src = imgSrctValue;
-    
-    galeryListContainer.prepend(galeryitemEl); 
-  }
+  const itemTemplate = document.querySelector("#galery-item-template").content;
+  const galeryitemEl = itemTemplate.querySelector('.galery__item').cloneNode(true);
+  const likeBtn = galeryitemEl.querySelector(".galery__item-like-btn");
 
+  galeryitemEl.querySelector(".galery__item-name").textContent = titleValue;
+  galeryitemEl.querySelector(".galery__item-img").src = imgSrctValue;
 
+  likeBtn.addEventListener("click",(evt)=>{
+    evt.target.classList.toggle('galery__item-like-btn_active');
+  })
+  galeryListContainer.prepend(galeryitemEl); 
+}
 
-
+rebderCards();
