8c8d2421aa8394fce604a256c7fe7b46568488f1
Avi Dalal <avi413@gmail.com>
2022-02-02 23:30:53 +0200

    feat(card): chabge galery item to card class

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
+
 const renderCards = () => {
   const initialCards = [
     {
@@ -130,43 +131,70 @@ function handleProfileFormSubmit(evt) {
     closePopup(popupProfile);
 }
 
-function handleDeleteGalleryCardItem(evt){
-  evt.target.closest('.gallery__item').remove();
-}
-function handleLikeBtn(evt){
-  evt.target.classList.toggle('gallery__item-like-btn_active');
-}
-
-function handleOpenImgPopup(evt){ 
-  popupImgTitleEl.textContent = evt.target.alt;
-  popupImgEl.src = evt.target.src;
-  popupImgEl.alt = evt.target.alt;
-  openPopup(popupImg);
-}
 
 function addGalleryCardItem(item) {
-  const galleryItemEl = createCard(item)
-  galleryListContainer.prepend(galleryItemEl); 
+  //const galleryItemEl = createCard(item);
+  const card = new Card(item, "#gallery-item-template");
+  const cardElement = card.generateCard();
+
+ // galleryListContainer.prepend(galleryItemEl); 
+  galleryListContainer.prepend(cardElement); 
 }
 
-function createCard(item) {
-  const itemTemplate      = document.querySelector("#gallery-item-template").content;
-  const galleryItemEl     = itemTemplate.querySelector(".gallery__item").cloneNode(true);
-  const likeBtn           = galleryItemEl.querySelector(".gallery__item-like-btn");
-  const trashBtn          = galleryItemEl.querySelector(".gallery__item-trash-btn");
-  const galleryImg        = galleryItemEl.querySelector(".gallery__item-img");
-  const galleryItemName   = galleryItemEl.querySelector(".gallery__item-name");
 
-  galleryItemName.textContent = item.name;
-  galleryImg.src = item.link;
-  galleryImg.alt = item.name;
+class Card {
+  constructor(data, cardSelector) {
+    this._text          = data.name;
+    this._img            = data.link;
+    this._cardSelector  = cardSelector;
+  }
 
-  likeBtn.addEventListener("click",handleLikeBtn)
-  trashBtn.addEventListener("click",handleDeleteGalleryCardItem)
-  galleryImg.addEventListener("click", handleOpenImgPopup);
+  _getTemplate() {
+    const cardElement = document
+      .querySelector(this._cardSelector)
+      .content
+      .querySelector(".gallery__item")
+      .cloneNode(true);
 
+    return cardElement;
+  }
 
-  return galleryItemEl;
+  generateCard() {
+    this._element = this._getTemplate();
+
+    this._element.querySelector(".gallery__item-img").src = this._img;
+    this._element.querySelector(".gallery__item-name").textContent = this._text;
+    this._setEventListeners();
+    return this._element;
+  }
+
+  _setEventListeners() {
+    this._element.querySelector(".gallery__item-like-btn").addEventListener("click", () => {
+      this._handleLikeBtn();
+    });
+    this._element.querySelector(".gallery__item-trash-btn").addEventListener("click", () => {
+      this._handleDeleteGalleryCardItem();
+    });
+    this._element.querySelector(".gallery__item-img").addEventListener("click",  () => {
+      this._handleOpenImgPopup();
+    });
+  }
+
+  _handleLikeBtn() {
+    this._element.querySelector(".gallery__item-like-btn").classList.toggle('gallery__item-like-btn_active');
+  }
+
+  _handleDeleteGalleryCardItem() {
+    this._element.querySelector(".gallery__item-trash-btn").closest('.gallery__item').remove();
+  }
+
+  _handleOpenImgPopup(){
+    const itemImg = this._element.querySelector(".gallery__item-img");
+    popupImgTitleEl.textContent = itemImg.alt;
+    popupImgEl.src = itemImg.src;
+    popupImgEl.alt = itemImg.alt;
+    openPopup(popupImg);
+  }
 }
 
 renderCards();
