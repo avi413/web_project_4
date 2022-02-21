25bcbd9ed57fe1d8427d746071312193af8b97df
Avi Dalal <avi413@gmail.com>
2022-02-21 09:13:15 +0200

    css mini

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
-    publicPath: ""
+    publicPath: "",
   },
   target: ['web', 'es5'],
   stats: { children: true },
@@ -34,8 +34,11 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: "css-loader"
+            loader: "css-loader",
+            options: { importLoaders: 1 }
           },
+          // add postcss-loader
+          "postcss-loader"
         ]
       },
       {
